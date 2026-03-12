7ce5646ba764739cad76fa38000cf7a16222dd72
JeongYuri <2024184029@tukorea.ac.kr>
2026-03-12 11:07:40 +0900

    함수 구조 정리
    함수 선언 먼저, 전역변수 말고 포인터로 main 변수 가져오기

--- Assignment.cpp
+++ Assignment.cpp
@@ -5,25 +5,37 @@ using namespace std;
 //주머니에 45개의 공이 있습니다.
 //그중 6개를 뽑는 프로그램을 만들어 보세요.
 
+void Initialize(int* Pocket);
+void Shuffle(int* Pocket);
+void Pick(int* Pocket);
+
+int main()
+{	
+	int Pocket[45] = { 0, };
+
+	int Picked[6];
+
+	Initialize(Pocket);
+
+	Shuffle(Pocket);
+
+	Pick(Pocket);
+
+	return 0;
+}
+
 void Initialize(int* Pocket)
 {
+	srand(unsigned(time(NULL)));
+
 	for (int i = 0; i < 45; ++i)
 	{
 		Pocket[i] = i + 1;
 	}
 }
 
-void RandomBalls(int* Pocket)
+void Shuffle(int* Pocket)
 {
-	//// 랜덤하게 공 섞기
-	//for (int i = 0; i < 45; i++)
-	//{
-	//	Pocket[i] = rand() % 45 + 1;
-	//}
-
-	// -----------------
-
-	// 강사님 코드
 	for (int i = 0; i < 45 * 100; i++)
 	{
 		int FirstIndex = rand() % 45;
@@ -35,47 +47,11 @@ void RandomBalls(int* Pocket)
 	}
 }
 
-void PickBalls(int* Pocket)
+void Pick(int* Pocket)
 {
-	//// 주머니에서 6개의 랜덤 공
-	//for (int i = 0; i < 6; i++)
-	//{
-	//	Picked[i] = Pocket[i] % 45 + 1;
-	//	for (int j = 0; j < i; j++)
-	//	{
-	//		if (Picked[i] == Picked[j]) // 중복 공 뽑지 않음
-	//		{
-	//			i--;
-	//			break;
-	//		}
-	//	}
-	//}
-
-	// -----------------
-
-	// 뽑은 공 6개 출력하기
 	cout << "뽑은 공들: ";
 	for (int i = 0; i < 6; i++)
 	{
 		cout << Pocket[i] << ' ';
 	}
-}
-
-int main()
-{
-	srand(unsigned(time(NULL)));
-	
-	int Pocket[45] = { 0, };
-	int* B = Pocket;
-
-	int Picked[6];
-	int* P = Picked;
-
-	Initialize(Pocket);
-
-	RandomBalls(Pocket);
-
-	PickBalls(Pocket);
-
-	return 0;
 }
